c3e6d3c15ff12ef1e41481cea6407426efd1df3c
aravinda-gamage <aravindagamage1998@gmail.com>
2024-03-14 20:50:05 +0530

    detailForm

---
+++ src/main/java/lk/ijse/ormCoursework/controller/DetailFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.ormCoursework.controller;
+
+import javafx.event.ActionEvent;
+
+public class DetailFormController {
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
