7049e621265352fca26075941182ff3bb289dd52
aravinda-gamage <aravindagamage1998@gmail.com>
2024-03-14 23:01:43 +0530

    userForm

--- src/main/java/lk/ijse/ormCoursework/controller/DetailFormController.java
+++ src/main/java/lk/ijse/ormCoursework/controller/DetailFormController.java
@@ -1,8 +1,25 @@
 package lk.ijse.ormCoursework.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class DetailFormController {
+    public TableView tblDetail;
+    public TableColumn colId;
+    public TableColumn colGetDate;
+    public TableColumn colReturnDate;
+    public TableColumn colUserId;
+    public TableColumn colBookId;
+    public JFXTextField txtId;
+    public DatePicker dpGetDate;
+    public DatePicker dpReturnDate;
+    public JFXComboBox cmbUserId;
+    public JFXComboBox cmbBookId;
+
     public void btnSaveOnAction(ActionEvent actionEvent) {
     }
 
---
+++ src/main/java/lk/ijse/ormCoursework/controller/UserFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.ormCoursework.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class UserFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtUserName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+    public JFXTextField txtRePassword;
+    public TableView tblBook;
+    public TableColumn colId;
+    public TableColumn colUserName;
+    public TableColumn colEmail;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
